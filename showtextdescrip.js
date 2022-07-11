e0e2cac8ae5dc205e3e4b7fc7239a46d61e41ef6
Adriel <adriel.castelo@hotmail.com>
2022-07-11 17:58:20 -0300

    inicial commit

---
+++ showtextdescrip.js
@@ -0,0 +1,250 @@
+function showEnsayar(){
+    document.getElementById('description-ensayar').style.display = 'block';
+    document.getElementById('description-ensayar').style.color = '#fff';
+    document.getElementById('description-ensayar').style.transition = '1s';
+    document.querySelector('.service .item-service i').style.color = '#000'
+    document.getElementById('ensayar-p').style.color= '#fff'
+    document.getElementById('ensayar-p').style.transition= '1s'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+}
+  function showGrabar(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'block';
+    document.getElementById('grabar-p').style.color= '#fff'
+    document.getElementById('grabar-p').style.transition= '1s'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+}
+  function showPartitura(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'block';
+    document.getElementById('partitura-p').style.color= '#fff'
+    document.getElementById('partitura-p').style.transition= '1s'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+
+}
+  function showLegal(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'block';
+    document.getElementById('legal-p').style.color= '#fff'
+    document.getElementById('legal-p').style.transition= '1s'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+
+}
+  function showVideoclip(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'block';
+    document.getElementById('videoclip-p').style.color= '#fff'
+    document.getElementById('videoclip-p').style.transition= '1s'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+}
+  function showFotograficas(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'block';
+    document.getElementById('fotograficas-p').style.color= '#fff'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+
+    document.getElementById('description-composicion').style.display = 'none';
+    document.getElementById('composicion-p').style.color= '#000'
+    document.getElementById('composicion-p').style.transition= '1s'
+    document.getElementById('composicion-p').style.opacity= '1'
+}
+  function showComposicion(){
+    document.getElementById('description-ensayar').style.display = 'none';
+    document.getElementById('ensayar-p').style.color= '#000'
+    document.getElementById('ensayar-p').style.transition= '1s'
+    document.getElementById('ensayar-p').style.opacity= '1'
+
+    document.getElementById('description-grabar').style.display = 'none';
+    document.getElementById('grabar-p').style.color= '#000'
+    document.getElementById('grabar-p').style.transition= '1s'
+    document.getElementById('grabar-p').style.opacity= '1'
+
+    document.getElementById('description-partitura').style.display = 'none';
+    document.getElementById('partitura-p').style.color= '#000'
+    document.getElementById('partitura-p').style.transition= '1s'
+    document.getElementById('partitura-p').style.opacity= '1'
+
+    document.getElementById('description-legal').style.display = 'none';
+    document.getElementById('legal-p').style.color= '#000'
+    document.getElementById('legal-p').style.transition= '1s'
+    document.getElementById('legal-p').style.opacity= '1'
+
+    document.getElementById('description-videoclip').style.display = 'none';
+    document.getElementById('videoclip-p').style.color= '#000'
+    document.getElementById('videoclip-p').style.transition= '1s'
+    document.getElementById('videoclip-p').style.opacity= '1'
+
+    document.getElementById('description-fotograficas').style.display = 'none';
+    document.getElementById('fotograficas-p').style.color= '#000'
+    document.getElementById('fotograficas-p').style.transition= '1s'
+    document.getElementById('fotograficas-p').style.opacity= '1'
+
+    document.getElementById('description-composicion').style.display = 'block';
+    document.getElementById('composicion-p').style.color= '#fff'
+    document.getElementById('composicion-p').style.transition= '1s'
+}
